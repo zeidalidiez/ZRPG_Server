97ca755a00c631c4265ca92d12d34e52e94e7869
Zeid Diez <69106534+zeidalidiez@users.noreply.github.com>
2020-12-21 18:15:30 -0800

    heroku

--- .history/src/app_20201221175619.js
+++ .history/src/app_20201221175619.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const BookRouter = require('./books/bookrouter.js');
-// const knex = require('knex');
+const knex = require('knex');
 
 const app = express();
 
-// const knexInstance = knex({
-//     client: 'pg',
-//     connection: process.env.DB_URL
-// });
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+});
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
@@ -22,19 +22,21 @@ app.use(cors());
 
 app.use('/books', BookRouter);
 
-// app.get('/', (req, res) => {
-//   knexInstance.from('*').select('*')
-//    .then(result => {
-//      console.log(result)
-//    })
-//   res.send()});
+app.get('/', (req, res) => {
+  knexInstance.from('*').select('*')
+   .then(result => {
+     console.log(result)
+   })
+  res.send()});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
     console.error(error);
     response = { message: error.message, error };
-
+  }
   res.status(500).json(response);
 });
 
