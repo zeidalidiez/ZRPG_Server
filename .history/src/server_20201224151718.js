aa89d394b727c6504aa79a2b32ed9d2b134c0598
Zeid Diez <69106534+zeidalidiez@users.noreply.github.com>
2020-12-24 15:35:46 -0800

    zzl

--- .history/src/server_20201224151718.js
+++ .history/src/server_20201224151718.js
@@ -4,21 +4,7 @@ const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    dbname='d5m5v2m0gmoil4',
-    host='ec2-52-6-75-198.compute-1.amazonaws.com',
-    port='5432',
-    user='znpkqzrrrahlwk',
-    password='3c3e3e8881f885e80d9d4d6e6cfc94ed5aa599144afa4ba95c5ff30bba42e0b2',
-    sslmode='require'
-  }
-
-
-
-
-
-
-
+  connection: DATABASE_URL
 
 });
 
