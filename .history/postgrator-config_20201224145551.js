bf8736405d9e1242bfa67844bb600322fb2371cc
Zeid Diez <69106534+zeidalidiez@users.noreply.github.com>
2020-12-24 14:56:53 -0800

    pgs

--- .history/postgrator-config_20201224145551.js
+++ .history/postgrator-config_20201224145551.js
@@ -7,5 +7,5 @@ module.exports = {
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
         //deploy prep
-        // ssl: {rejectUnauthorized: true}
+        ssl: {rejectUnauthorized: true}
 }
